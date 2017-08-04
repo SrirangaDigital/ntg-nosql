3b034846d2232ebbabf70b6866fb58e0b02cfe49
suresh07 <soori_rozee@yahoo.com>
2017-08-04 18:41:29 +0530

    Project: Header and footer updated

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,8 +4,9 @@
             <div class="row"><!-- row -->
                 <div class="col-lg-12">
                     <p class="copy-text text-center">
-						© 2016 JSS Mahavidyapeetha Mysuru.<br />
-                        No image available from this site may be used for commercial purposes without written permission from JSS Mahavidyapeetha.
+						© 2017 - All Rights Reserved - The Natarang Pratishthan<br />
+                        No image available from this site may be used for commercial purposes without written permission from The Natarang Pratishthan.
+
                     </p>
                 </div>
             </div><!-- row end -->
@@ -15,7 +16,7 @@
     <!-- End Main page layout
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js" async></script>
-    <script type="text/javascript" src="<?=PUBLIC_URL?>js/imagesloaded.pkgd.min.js"></script>
+	<script type="text/javascript" src="<?=PUBLIC_URL?>js/imagesloaded.pkgd.min.js"></script>
     <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js" async></script>
    
 </body>
--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
     <!-- Basic Page Needs
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
-    <title><?php if($pageTitle) echo $pageTitle . ' | '; ?>JSS Mahavidyapeetha</title>
+    <title><?php if($pageTitle) echo $pageTitle . ' | '; ?>Natarang Pratishthan - Archive and Research Centre for Indian Theatre</title>
     <meta name="description" content="">
     <meta name="author" content="">
 
@@ -63,13 +63,13 @@
             </div>
             <div class="collapse navbar-collapse" id="navbar-primary-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="<?=BASE_URL?>#home">Mahavidyapeetha</a></li>
+                    <li class="active"><a href="<?=BASE_URL?>#home">Natarang Pratishthan</a></li>
                     <li><a>·</a></li>
 <!--                     <li><a href="<?=BASE_URL?>#about">About</a></li>
                     <li><a>·</a></li>
  -->                    <li><a href="<?=BASE_URL?>#collection">Collection</a></li>
-                    <li><a href="#"><img src="<?=PUBLIC_URL?>images/logo.png" alt="Logo of the JSS Mahavidyapeetha" class="img-circle logo"></a></li>
-                    <li><a href="<?=BASE_URL?>#physical">News and Events</a></li>
+                    <li><a href="#"><img src="<?=PUBLIC_URL?>images/logo.png" alt="Logo of Natarang Pratisthan" class="img-circle logo"></a></li>
+                    <li><a href="<?=BASE_URL?>#">News and Events</a></li>
                     <li><a>·</a></li>
                     <li><a href="<?=BASE_URL?>#contact">Contact</a></li>
 					<li><a>·</a></li>
