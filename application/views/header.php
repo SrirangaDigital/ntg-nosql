c79a249ab0d5cdefcb2b9880ada474a61d6e044e
root <root@localhost.localdomain>
2017-08-04 21:10:04 +0530

    Project: UI fix

--- application/views/header.php
+++ application/views/header.php
@@ -32,7 +32,7 @@
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> -->
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/navbar.css">
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/carousel.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/page.css">
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/page.css?v=1.0">
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/archive.css">
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/general.css">
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/flat.css">
