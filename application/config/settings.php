60d0b1f197835b46a5ee4f4fa680660b9e9ba16a
suresh07 <soori_rozee@yahoo.com>
2017-08-04 18:46:09 +0530

    Project: SALT removed

--- application/config/settings.php
+++ application/config/settings.php
@@ -14,7 +14,6 @@ define('PER_PAGE', 10);
 define('PHOTO_FILE_EXT', '.JPG');
 
 // user settings (login and registration)
-define('SALT', 'jssArchives');
 define('REQUIRE_EMAIL_VALIDATION', False);//Set these values to True only
 define('REQUIRE_RESET_PASSWORD', False);//if outbound mails can be sent from the server
 define('REQUIRE_GIT_TRACKING', False);
