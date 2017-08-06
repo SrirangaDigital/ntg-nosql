a2ab8faca04ced975e72c808d472b1157f9beb9e
suresh07 <soori_rozee@yahoo.com>
2017-08-06 17:26:12 +0530

    Core: Acession cards displayed if exists

--- application/views/footer.php
+++ application/views/footer.php
@@ -17,7 +17,7 @@
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js" async></script>
 	<script type="text/javascript" src="<?=PUBLIC_URL?>js/imagesloaded.pkgd.min.js"></script>
-    <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js" async></script>
+    <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js?v=1.0" async></script>
    
 </body>
 </html>
