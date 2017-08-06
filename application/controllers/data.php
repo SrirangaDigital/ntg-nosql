12e3a755e783e714c30e0aa8ba27c00f556abf8b
suresh07 <soori_rozee@yahoo.com>
2017-08-06 16:12:57 +0530

    Project: typo fixed in state name

--- application/controllers/data.php
+++ application/controllers/data.php
@@ -50,33 +50,21 @@ class data extends Controller {
 	// Use this method for global changes in json files
 	public function modify() {
 
-		$jsonFiles = $this->model->getFilesIteratively(PHY_METADATA_URL . '003/', $pattern = '/index.json$/i');
+		$jsonFiles = $this->model->getFilesIteratively(PHY_METADATA_URL, $pattern = '/index.json$/i');
 		
 		foreach ($jsonFiles as $jsonFile) {
 
-			$parentId = preg_replace('/.*003\/(.*)\/\d{5}\/.*/', "$1", $jsonFile);
-			
-			$parentJsonFile = PHY_FOREIGN_KEYS_URL . 'EventID/' . $parentId . '.json';
-			$parentJsonFileOut = PHY_METADATA_URL . 'foreign/' . $parentId . '.json';
-			
 			$contentString = file_get_contents($jsonFile);
 			$content = json_decode($contentString, true);
 
-			$parentContentString = file_get_contents($parentJsonFile);
-			$parentContent = json_decode($parentContentString, true);
+			if(isset($content['State'])) {
 
-			if((!isset($content['State'])) && (isset($parentContent['Place']))) {
+				if(($content['State'] == 'चंडीगढ़†')) {
 
-				if(($parentContent['Place'] == 'पांडिचेरी')) {
-
-					$content['State'] = $parentContent['Place'];
-					unset($parentContent['Place']);
+					$content['State'] = 'चंडीगढ़';
 		
 					$json = json_encode($content, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-					$parentJson = json_encode($parentContent, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-
 					file_put_contents($jsonFile, $json);
-					file_put_contents($parentJsonFileOut, $parentJson);
 				}
 			}
 		}
