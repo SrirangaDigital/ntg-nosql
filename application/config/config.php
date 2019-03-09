c7e76b6917b4bfa14ca4c9b08d108f0f8e237f11
Suresh <soori_rozee@yahoo.com>
2019-03-09 18:02:54 +0530

    Sriranga deploy user added for git operations

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://192.168.1.101/ntg-nosql/');
+define('BASE_URL', 'http://localhost/ntg-nosql/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('DATA_URL', PUBLIC_URL . 'data/');
 define('METADATA_URL', PUBLIC_URL . 'metaData/');
@@ -26,10 +26,10 @@ define('DB_USER', 'ntgUSER');
 define('DB_PASSWORD', 'ntg123');
 
 // Git config
-define('GIT_USER_NAME', 'shruthisdst');
-define('GIT_PASSWORD', 'shruthitr14');
+define('GIT_USER_NAME', 'sriranga-deploy');
+define('GIT_PASSWORD', 'xxx');
 define('GIT_REPO', 'github.com/SrirangaDigital/ntg-nosql.git');
 define('GIT_REMOTE', 'https://' . GIT_USER_NAME . ':' . GIT_PASSWORD . '@' . GIT_REPO);
-define('GIT_EMAIL', 'shruthitr.nayak@gmail.com');
+define('GIT_EMAIL', 'accounts@srirangadigital.com');
 
 ?>
